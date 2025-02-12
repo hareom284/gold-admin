7359e55c1976040f709f7338e19658858f32de4b
Aung-Thu-Heain <mrfoethu@gmail.com>
2025-02-13 02:24:08 +0630

    twilo testing

---
+++ Modules/Frontend/Routes/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Frontend\Http\Controllers\Auth\OTPController;
+use Modules\Frontend\Http\Controllers\Auth\AuthController;
+
+// Login with OTP
+Route::get('/login', [OTPController::class, 'otpLogin'])->name('login');
+Route::post('/auth/otp-login-store', [OTPController::class, 'otpLoginStore'])->name('auth.otp-login-store');
+Route::get('/auth/check-user-exists', [OTPController::class, 'checkUserExists'])->name('check.user.exists');
+
+
+// Login with Google
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback'])->name('auth.google.callback');
+
+
+Route::get('/send-otp',[OTPController::class,'sendOTP'])->name('send.otp');
