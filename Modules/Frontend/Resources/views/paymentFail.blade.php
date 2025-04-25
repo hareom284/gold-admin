0cbc840750d8a16ba98a4021feced7831af589e1
Aung-Thu-Heain <mrfoethu@gmail.com>
2025-04-25 11:20:59 +0630

    add redirect paymens success

---
+++ Modules/Frontend/Resources/views/paymentFail.blade.php
@@ -0,0 +1,6 @@
+@extends('frontend::layouts.master')
+@section('content')
+<div class="section-spacing-bottom bg-white d-flex justify-content-center align-items-center flex-column p-5">
+    {{$orderId}}
+</div>
+@endsection
