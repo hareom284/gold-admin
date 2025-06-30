e4d4fbd53f2bea9c2fc7e989d1a335b1f8602332
Aung-Thu-Heain <mrfoethu@gmail.com>
2025-06-30 14:38:03 +0630

    fix session error

--- config/cors.php
+++ config/cors.php
@@ -1,37 +1,34 @@
 <?php
 
 return [
+
     /*
-     * |--------------------------------------------------------------------------
-     * | Cross-Origin Resource Sharing (CORS) Configuration
-     * |--------------------------------------------------------------------------
-     * |
-     * | Here you may configure your settings for cross-origin resource sharing
-     * | or "CORS". This determines what cross-origin operations may execute
-     * | in web browsers. You are free to adjust these settings as needed.
-     * |
-     * | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-     * |
-     */
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie', 'app/media-library/upload','*'],
-    'paths' => ['*'],
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'app/media-library/upload'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'capacitor://localhost',
-        'http://localhost',
-        'https://localhost',
-        'http://127.0.0.1',
-        'https://127.0.0.1',
-        'http://127.0.0.1:8100',
-        'https://127.0.0.1:8100',
-        'http://localhost:8100',
-        'https://localhost:8100',
-        'http://localhost:5173',
-        env('ALLOW_URL', '*')
-    ],
+
+    'allowed_origins' => ['*'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
+
 ];
